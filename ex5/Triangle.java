0edae40523fbb6da99e0c820a5ab1a8e35572f67
Désiré <github.freebie042@passmail.net>
2024-01-09 18:13:04 +0100

    build(exerice): Exercice n°5

--- ex5/Triangle.java
+++ ex5/Triangle.java
@@ -23,14 +23,21 @@ public class Triangle {
         double s = calculerPerimetre() / 2;
         return Math.sqrt(s * (s - premiereCote) * (s - secondCote) * (s - troisiemeCote));
     }
+
+    public void deplacerTriangle(int axeX, int axeY, int axeZ) {
+        System.out.println("Déplacement du triangle sur les axes: " + "x= " + axeX + ", " + "y= " + axeY + ", " + "z= " + axeZ + ".");
+    }
     
 
     public static void main(String[] args) {
         // Création d'objets
         Triangle triangle1 = new Triangle(14, 7, 18);
 
-        //Affichage
+        //Affichage Perimetre
         System.out.println(triangle1.calculerPerimetre());
+        // Affichage Aire
         System.out.println(triangle1.calculerAire());
+        //Affichage Deplacement
+        triangle1.deplacerTriangle(2, 3, 6);
     }
 }
